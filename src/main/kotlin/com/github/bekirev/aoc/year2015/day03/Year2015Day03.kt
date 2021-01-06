62b8290348ad4d1822710fb6fad0d65708d40ce9
artechry <abekirev@gmail.com>
2021-01-06 23:24:41 +0300

    Year 2015 Day 3 Part 1

---
+++ src/main/kotlin/com/github/bekirev/aoc/year2015/day03/Year2015Day03.kt
@@ -0,0 +1,59 @@
+package com.github.bekirev.aoc.year2015.day03
+
+import com.github.bekirev.aoc.day.Day
+import com.github.bekirev.aoc.year2015.Year2015Day
+import com.github.bekirev.aoc.year2015.day03.Direction2D.EAST
+import com.github.bekirev.aoc.year2015.day03.Direction2D.NORTH
+import com.github.bekirev.aoc.year2015.day03.Direction2D.SOUTH
+import com.github.bekirev.aoc.year2015.day03.Direction2D.WEST
+
+fun main() = Day.run(Year2015Day03)
+
+object Year2015Day03 : Year2015Day(3) {
+    override fun first(input: String): Int =
+        Position2D.ZERO.positions(input.asDirection2DSequence()).distinct().count()
+}
+
+enum class Direction2D {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST,
+    ;
+}
+
+fun Char.toDirection2D(): Direction2D = when (this) {
+    '^' -> NORTH
+    'v' -> SOUTH
+    '>' -> EAST
+    '<' -> WEST
+    else -> throw IllegalArgumentException()
+}
+
+fun String.asDirection2DSequence(): Sequence<Direction2D> =
+    asSequence().map(Char::toDirection2D)
+
+data class Position2D(
+    val x: Int,
+    val y: Int,
+) {
+    companion object {
+        val ZERO = Position2D(0, 0)
+    }
+}
+
+fun Position2D.move(direction2D: Direction2D): Position2D = when (direction2D) {
+    NORTH -> copy(y = y + 1)
+    SOUTH -> copy(y = y - 1)
+    EAST -> copy(x = x + 1)
+    WEST -> copy(x = x - 1)
+}
+
+fun Position2D.positions(directions: Sequence<Direction2D>): Sequence<Position2D> = sequence {
+    var position = this@positions
+    yield(position)
+    for (direction in directions) {
+        position = position.move(direction)
+        yield(position)
+    }
+}
---
+++ src/test/kotlin/com/github/bekirev/aoc/year2015/day03/Year2015Day03Test.kt
@@ -0,0 +1,80 @@
+package com.github.bekirev.aoc.year2015.day03
+
+import com.github.bekirev.aoc.year2015.day03.Direction2D.EAST
+import com.github.bekirev.aoc.year2015.day03.Direction2D.NORTH
+import com.github.bekirev.aoc.year2015.day03.Direction2D.SOUTH
+import com.github.bekirev.aoc.year2015.day03.Direction2D.WEST
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.data.blocking.forAll
+import io.kotest.data.row
+import io.kotest.matchers.ints.shouldBeExactly
+import io.kotest.matchers.shouldBe
+
+class Year2015Day03Test : ShouldSpec({
+    should("determine number of positions visited") {
+        forAll(
+            row("", 1),
+            row(">", 2),
+            row("^>v<", 4),
+            row("^v^v^v^v^v", 2),
+            row("^>^>vv<<^^", 9),
+        ) { input, result ->
+            Year2015Day03.first(input) shouldBeExactly result
+        }
+    }
+    should("convert char to direction") {
+        forAll(
+            row('^', NORTH),
+            row('v', SOUTH),
+            row('>', EAST),
+            row('<', WEST),
+        ) { char, direction ->
+            char.toDirection2D() shouldBe direction
+        }
+    }
+    should("convert string to sequence of directions") {
+        forAll(
+            row("", emptyList()),
+            row(">", listOf(EAST)),
+            row("><", listOf(EAST, WEST)),
+            row("vv<^>", listOf(SOUTH, SOUTH, WEST, NORTH, EAST)),
+        ) { str, directions ->
+            str.asDirection2DSequence().toList() shouldBe directions
+        }
+    }
+    should("move position according to given direction") {
+        forAll(
+            row(Position2D(0, 0), NORTH, Position2D(0, 1)),
+            row(Position2D(2, 1), NORTH, Position2D(2, 2)),
+            row(Position2D(-1, 2), SOUTH, Position2D(-1, 1)),
+            row(Position2D(0, 0), SOUTH, Position2D(0, -1)),
+            row(Position2D(0, 0), EAST, Position2D(1, 0)),
+            row(Position2D(5, 10), EAST, Position2D(6, 10)),
+            row(Position2D(3, 2), WEST, Position2D(2, 2)),
+            row(Position2D(0, 0), WEST, Position2D(-1, 0)),
+        ) { position, direction, result ->
+            position.move(direction) shouldBe result
+        }
+    }
+    should("build a sequence of all visited positions") {
+        forAll(
+            row(
+                Position2D(1, 2),
+                emptySequence(),
+                listOf(Position2D(1, 2))
+            ),
+            row(
+                Position2D(3, 2),
+                sequenceOf(WEST),
+                listOf(Position2D(3, 2), Position2D(2, 2))
+            ),
+            row(
+                Position2D(1, 1),
+                sequenceOf(NORTH, EAST, SOUTH, WEST),
+                listOf(Position2D(1, 1), Position2D(1, 2), Position2D(2, 2), Position2D(2, 1), Position2D(1, 1))
+            ),
+        ) { startPosition, directions, positions ->
+            startPosition.positions(directions).toList() shouldBe positions
+        }
+    }
+})
