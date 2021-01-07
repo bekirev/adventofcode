adcb7d3822b80b282240a758c0220ea86cf58ccf
artechry <abekirev@gmail.com>
2021-01-07 16:17:42 +0300

    Year 2015 Day 5 Part 2

--- src/main/kotlin/com/github/bekirev/aoc/year2015/day05/Year2015Day05.kt
+++ src/main/kotlin/com/github/bekirev/aoc/year2015/day05/Year2015Day05.kt
@@ -2,69 +2,33 @@ package com.github.bekirev.aoc.year2015.day05
 
 import com.github.bekirev.aoc.day.Day
 import com.github.bekirev.aoc.year2015.Year2015Day
-import java.util.LinkedList
 
 fun main() = Day.run(Year2015Day05)
 
 object Year2015Day05 : Year2015Day(5) {
-    override fun first(input: String): Int {
-        return input.splitToSequence("\n")
+    private val AA_REGEX = "([a-z])\\1".toRegex()
+    private val BAD_2_CHARS_REGEX = "ab|cd|pq|xy".toRegex()
+    private val ABAB_REGEX = "([a-z]{2}).*\\1".toRegex()
+    private val ABA_REGEX = "([a-z]).\\1".toRegex()
+
+    override fun first(input: String): Int =
+        count { string ->
+            string.count { it in "aeiou" } >= 3 &&
+                    AA_REGEX in string &&
+                    BAD_2_CHARS_REGEX !in string
+        }
+
+    override fun second(input: String): Int =
+        count { string ->
+            ABAB_REGEX in string &&
+                    ABA_REGEX in string
+        }
+
+    private fun count(
+        predicate: (String) -> Boolean,
+    ): Int =
+        input
+            .lineSequence()
             .filter(String::isNotBlank)
-            .count(String::isNice)
-    }
-}
-
-fun String.isNice(): Boolean = containsAtLeastThreeVowels() &&
-        containsRepeatingTwiceInARowLetter() &&
-        !containsUglyStrings()
-
-fun String.containsAtLeastThreeVowels(): Boolean {
-    var vowelsCount = 0
-    for (char in this) {
-        if (char.isVowel()) ++vowelsCount
-        if (vowelsCount >= 3) return true
-    }
-    return false
-}
-
-fun Char.isVowel(): Boolean = this in setOf('a', 'e', 'i', 'o', 'u')
-
-fun String.containsRepeatingTwiceInARowLetter(): Boolean {
-    var prev: Char? = null
-    for (char in this) {
-        if (char == prev) return true
-        prev = char
-    }
-    return false
-}
-
-val uglyStrings = setOf("ab", "cd", "pq", "xy")
-
-fun String.containsUglyStrings(): Boolean {
-    val sizes = uglyStrings.asSequence().map { it.length }.toSet()
-    val maxSize = sizes.maxOrNull() ?: return false
-    val currentSubStringsOfSize = mutableMapOf<Int, String>()
-    val prevChars = LinkedList<Char>()
-    fun fillSubstrings() {
-        currentSubStringsOfSize.clear()
-        val sb = StringBuilder()
-        for (c in prevChars) {
-            sb.append(c)
-            if (sb.length in sizes)
-                currentSubStringsOfSize[sb.length] = sb.toString()
-        }
-    }
-    for (char in this) {
-        when (prevChars.size) {
-            maxSize -> {
-                prevChars.remove()
-                prevChars.add(char)
-            }
-            else -> prevChars.add(char)
-        }
-        fillSubstrings()
-        if (uglyStrings.any(currentSubStringsOfSize::containsValue))
-            return true
-    }
-    return false
+            .count(predicate)
 }
--- src/test/kotlin/com/github/bekirev/aoc/year2015/day05/Year2015Day05Test.kt
+++ src/test/kotlin/com/github/bekirev/aoc/year2015/day05/Year2015Day05Test.kt
@@ -1,10 +1,7 @@
 package com.github.bekirev.aoc.year2015.day05
 
 import io.kotest.core.spec.style.ShouldSpec
-import io.kotest.data.blocking.forAll
-import io.kotest.data.row
 import io.kotest.matchers.ints.shouldBeExactly
-import io.kotest.matchers.shouldBe
 
 class Year2015Day05Test : ShouldSpec({
     should("determine number of nice strings") {
@@ -17,38 +14,15 @@ class Year2015Day05Test : ShouldSpec({
             """.trimIndent()
         Year2015Day05.first(input) shouldBeExactly 2
     }
-    should("determine whether a string is nice") {
-        forAll(
-            row("ugknbfddgicrmopn", true),
-            row("aaa", true),
-            row("jchzalrnumimnmhp", false),
-            row("haegwjzuvuyypxyu", false),
-            row("dvszwmarrgswjxmb", false),
-        ) { str, nice ->
-            str.isNice() shouldBe nice
-        }
-    }
-    should("check whether a string contains at least three vowels") {
-        forAll(
-            row("", false),
-            row("a", false),
-            row("nklseninm", false),
-            row("amkomsdnudsjk", true),
-            row("eua", true),
-            row("bnsajiokasju", true),
-        ) { str, result ->
-            str.containsAtLeastThreeVowels() shouldBe result
-        }
-    }
-    should("check whether a string contains repeating twice in a row letter") {
-        forAll(
-            row("", false),
-            row("j", false),
-            row("as", false),
-            row("gg", true),
-            row("asdbjnkkfsa", true)
-        ) { str, result ->
-            str.containsRepeatingTwiceInARowLetter() shouldBe result
-        }
+    should("count nice strings acoording to the second set of rules") {
+        val input = """
+            qjhvhtzxzqqjkmpb
+            xxyxx
+            uurcxstgmygtbstg
+            ieodomkazucvgmuy
+            aaa
+            aaaa
+            """.trimIndent()
+        Year2015Day05.second(input) shouldBeExactly 3
     }
 })
