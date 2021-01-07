a76a41fdd9c4fc348489a53ee4f70a6bf0b00332
artechry <abekirev@gmail.com>
2021-01-07 18:05:59 +0300

    Year 2015 Day 6 Part 2

--- src/main/kotlin/com/github/bekirev/aoc/year2015/day06/Year2015Day06.kt
+++ src/main/kotlin/com/github/bekirev/aoc/year2015/day06/Year2015Day06.kt
@@ -12,37 +12,73 @@ import com.github.bekirev.aoc.year2015.day06.LightAction.TURN_ON
 fun main() = Day.run(Year2015Day06)
 
 object Year2015Day06 : Year2015Day(6) {
-    override fun first(input: String): Int {
+    override fun first(input: String): Int =
+        finalGrid(input, SimpleStateChanger)
+            .getAll()
+            .count { it.value == 1 }
+
+    override fun second(input: String): Int =
+        finalGrid(input, BrightnessStateChanger)
+            .getAll()
+            .sumBy { it.value }
+
+    private fun finalGrid(input: String, stateChanger: StateChanger): MutableGrid<LightState> {
         val size = Size(1000, 1000)
-        val grid: MutableGrid<LightState> = ArrayListMutableGrid(size) { false }
+        val grid: MutableGrid<LightState> = ArrayListMutableGrid(size) { 0 }
         input
             .lineSequence()
             .filter(String::isNotBlank)
             .map(String::toLightCommand)
-            .forEach { execute(it, grid) }
-        return grid.getAll().count { it.value }
+            .forEach { execute(it, grid, stateChanger) }
+        return grid
     }
 }
 
-fun execute(command: LightCommand, grid: MutableGrid<LightState>) {
+fun execute(command: LightCommand, grid: MutableGrid<LightState>, stateChanger: StateChanger) {
     for (row in command.rowRange) {
         val getByColFun = grid[row]
         val setByColFun = grid.set(row)
         for (col in command.colRange) {
-            setByColFun(col, command.action.stateAfter(getByColFun(col)))
+            setByColFun(col, stateChanger.stateAfter(getByColFun(col), command.action))
         }
     }
 }
 
-typealias LightState = Boolean
+typealias LightState = Int
 
 data class LightCommand(val action: LightAction, val rowRange: IntRange, val colRange: IntRange)
 
-enum class LightAction(val stateAfter: (LightState) -> LightState) {
-    TURN_ON({ true }),
-    TURN_OFF({ false }),
-    TOGGLE({ state -> !state }),
+enum class LightAction {
+    TURN_ON,
+    TURN_OFF,
+    TOGGLE,
     ;
+}
+
+interface StateChanger {
+    fun stateAfter(state: LightState, action: LightAction): LightState
+}
+
+object SimpleStateChanger : StateChanger {
+    override fun stateAfter(state: LightState, action: LightAction): LightState = when (action) {
+        TURN_ON -> 1
+        TURN_OFF -> 0
+        TOGGLE -> when (state) {
+            0 -> 1
+            else -> 0
+        }
+    }
+}
+
+object BrightnessStateChanger : StateChanger {
+    override fun stateAfter(state: LightState, action: LightAction): LightState = when (action) {
+        TURN_ON -> state + 1
+        TURN_OFF -> when (state) {
+            0 -> 0
+            else -> state - 1
+        }
+        TOGGLE -> state + 2
+    }
 }
 
 fun String.toLightCommand(): LightCommand {
--- src/test/kotlin/com/github/bekirev/aoc/year2015/day06/Year2015Day06.kt
+++ src/test/kotlin/com/github/bekirev/aoc/year2015/day06/Year2015Day06.kt
@@ -9,16 +9,31 @@ import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class Year2015Day06 : ShouldSpec({
-    should("Determine next state") {
+    should("Determine next state with boolean-like state") {
         forAll(
-            row(TURN_ON, false, true),
-            row(TURN_ON, true, true),
-            row(TURN_OFF, false, false),
-            row(TURN_OFF, true, false),
-            row(TOGGLE, false, true),
-            row(TOGGLE, true, false),
+            row(TURN_ON, 0, 1),
+            row(TURN_ON, 1, 1),
+            row(TURN_OFF, 0, 0),
+            row(TURN_OFF, 1, 0),
+            row(TOGGLE, 0, 1),
+            row(TOGGLE, 1, 0),
         ) { action, state, result ->
-            action.stateAfter(state) shouldBe result
+            SimpleStateChanger.stateAfter(state, action) shouldBe result
+        }
+    }
+    should("Determine next state with brightness state") {
+        forAll(
+            row(TURN_ON, 0, 1),
+            row(TURN_ON, 1, 2),
+            row(TURN_ON, 2, 3),
+            row(TURN_OFF, 0, 0),
+            row(TURN_OFF, 1, 0),
+            row(TURN_OFF, 2, 1),
+            row(TOGGLE, 0, 2),
+            row(TOGGLE, 1, 3),
+            row(TOGGLE, 2, 4),
+        ) { action, state, result ->
+            BrightnessStateChanger.stateAfter(state, action) shouldBe result
         }
     }
 })
