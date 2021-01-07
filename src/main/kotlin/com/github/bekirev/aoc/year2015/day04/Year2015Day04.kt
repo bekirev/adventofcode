839910b41c81e446540b1ab3de4023a706ee9dcd
artechry <abekirev@gmail.com>
2021-01-07 09:45:35 +0300

    Year 2015 Day 4 Part 2

--- src/main/kotlin/com/github/bekirev/aoc/year2015/day04/Year2015Day04.kt
+++ src/main/kotlin/com/github/bekirev/aoc/year2015/day04/Year2015Day04.kt
@@ -10,12 +10,25 @@ fun main() = Day.run(Year2015Day04)
 
 object Year2015Day04 : Year2015Day(4) {
     override fun first(input: String): Int {
-        val secretKey = input.trim()
-        return generateSequence(1, 1::plus)
-            .first { number ->
-                (secretKey + number).md5().startsWith("00000")
-            }
+        return result(input, 5)
     }
+
+    override fun second(input: String): Int {
+        return result(input, 6)
+    }
+
+    private fun result(input: String, leadingZerosCount: Int): Int {
+        val secretKey = input.trim()
+        return firstMd5HashWithLeadingZeros(secretKey, leadingZerosCount)
+    }
+}
+
+fun firstMd5HashWithLeadingZeros(secretKey: String, leadingZerosCount: Int): Int {
+    val prefix = "0".repeat(leadingZerosCount)
+    return generateSequence(1, 1::plus)
+        .first { number ->
+            (secretKey + number).md5().startsWith(prefix)
+        }
 }
 
 fun String.md5(): String =
